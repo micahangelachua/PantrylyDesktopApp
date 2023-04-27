acf647fff35fae5d3d5f1f40f2357ff7b1ae203f
Neil Francis M. Palmares <neilfrancispalmares@tutanota.com>
2023-04-27 20:01:22 +0800

    UserDashboard frontend additions, and event for adding pantries.

--- PantrylyDesktopApp/UserDashboard.Designer.cs
+++ PantrylyDesktopApp/UserDashboard.Designer.cs
@@ -36,19 +36,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.lbl_DasboardLogo = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_AddNewPantry = new System.Windows.Forms.Button();
+            this.pnl_Pantry = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.flp_PantriesContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnl_WinTitleAndControls = new System.Windows.Forms.Panel();
+            this.btn_DashboardMinimize = new System.Windows.Forms.Button();
+            this.btn_DashboardClose = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Checklist)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_UserPicture)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Pantry)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Dashboard)).BeginInit();
-            this.panel2.SuspendLayout();
+            this.pnl_Pantry.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.flp_PantriesContainer.SuspendLayout();
+            this.pnl_WinTitleAndControls.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -58,45 +64,40 @@
             this.panel1.Controls.Add(this.pb_UserPicture);
             this.panel1.Controls.Add(this.pb_Pantry);
             this.panel1.Controls.Add(this.pb_Dashboard);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(0, 71);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(212, 806);
+            this.panel1.Size = new System.Drawing.Size(159, 649);
             this.panel1.TabIndex = 0;
             // 
             // pb_Checklist
             // 
-            this.pb_Checklist.Location = new System.Drawing.Point(37, 540);
-            this.pb_Checklist.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb_Checklist.Location = new System.Drawing.Point(28, 439);
             this.pb_Checklist.Name = "pb_Checklist";
-            this.pb_Checklist.Size = new System.Drawing.Size(133, 114);
+            this.pb_Checklist.Size = new System.Drawing.Size(100, 93);
             this.pb_Checklist.TabIndex = 12;
             this.pb_Checklist.TabStop = false;
             // 
             // pb_UserPicture
             // 
-            this.pb_UserPicture.Location = new System.Drawing.Point(37, 37);
-            this.pb_UserPicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb_UserPicture.Location = new System.Drawing.Point(28, 30);
             this.pb_UserPicture.Name = "pb_UserPicture";
-            this.pb_UserPicture.Size = new System.Drawing.Size(133, 114);
+            this.pb_UserPicture.Size = new System.Drawing.Size(100, 93);
             this.pb_UserPicture.TabIndex = 9;
             this.pb_UserPicture.TabStop = false;
             // 
             // pb_Pantry
             // 
-            this.pb_Pantry.Location = new System.Drawing.Point(37, 400);
-            this.pb_Pantry.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb_Pantry.Location = new System.Drawing.Point(28, 325);
             this.pb_Pantry.Name = "pb_Pantry";
-            this.pb_Pantry.Size = new System.Drawing.Size(133, 114);
+            this.pb_Pantry.Size = new System.Drawing.Size(100, 93);
             this.pb_Pantry.TabIndex = 11;
             this.pb_Pantry.TabStop = false;
             // 
             // pb_Dashboard
             // 
-            this.pb_Dashboard.Location = new System.Drawing.Point(37, 256);
-            this.pb_Dashboard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pb_Dashboard.Location = new System.Drawing.Point(28, 208);
             this.pb_Dashboard.Name = "pb_Dashboard";
-            this.pb_Dashboard.Size = new System.Drawing.Size(133, 114);
+            this.pb_Dashboard.Size = new System.Drawing.Size(100, 93);
             this.pb_Dashboard.TabIndex = 10;
             this.pb_Dashboard.TabStop = false;
             // 
@@ -104,10 +105,9 @@
             // 
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(102)))), ((int)(((byte)(78)))));
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(240, 210);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(329, 46);
+            this.label1.Size = new System.Drawing.Size(250, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "ADD NEW PANTRY";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -117,10 +117,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(102)))), ((int)(((byte)(78)))));
-            this.label2.Location = new System.Drawing.Point(236, 135);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(179, 205);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(234, 56);
+            this.label2.Size = new System.Drawing.Size(187, 45);
             this.label2.TabIndex = 2;
             this.label2.Text = "PANTRIES";
             // 
@@ -129,45 +128,43 @@
             this.lbl_DasboardLogo.AutoSize = true;
             this.lbl_DasboardLogo.Font = new System.Drawing.Font("Pacifico", 32F);
             this.lbl_DasboardLogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(102)))), ((int)(((byte)(78)))));
-            this.lbl_DasboardLogo.Location = new System.Drawing.Point(1604, 11);
-            this.lbl_DasboardLogo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DasboardLogo.Location = new System.Drawing.Point(1203, 74);
             this.lbl_DasboardLogo.Name = "lbl_DasboardLogo";
-            this.lbl_DasboardLogo.Size = new System.Drawing.Size(233, 94);
+            this.lbl_DasboardLogo.Size = new System.Drawing.Size(198, 84);
             this.lbl_DasboardLogo.TabIndex = 3;
             this.lbl_DasboardLogo.Text = "Pantryly";
             // 
-            // button1
+            // btn_AddNewPantry
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(102)))), ((int)(((byte)(78)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(101, 70);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "+";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_AddNewPantry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(102)))), ((int)(((byte)(78)))));
+            this.btn_AddNewPantry.FlatAppearance.BorderSize = 0;
+            this.btn_AddNewPantry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_AddNewPantry.Location = new System.Drawing.Point(88, 99);
+            this.btn_AddNewPantry.Name = "btn_AddNewPantry";
+            this.btn_AddNewPantry.Size = new System.Drawing.Size(75, 23);
+            this.btn_AddNewPantry.TabIndex = 4;
+            this.btn_AddNewPantry.Text = "+";
+            this.btn_AddNewPantry.UseVisualStyleBackColor = false;
+            this.btn_AddNewPantry.Click += new System.EventHandler(this.btnAddNewPantry_Click);
             // 
-            // panel2
+            // pnl_Pantry
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(253, 256);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(303, 169);
-            this.panel2.TabIndex = 5;
+            this.pnl_Pantry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.pnl_Pantry.Controls.Add(this.label1);
+            this.pnl_Pantry.Controls.Add(this.btn_AddNewPantry);
+            this.pnl_Pantry.Location = new System.Drawing.Point(3, 3);
+            this.pnl_Pantry.Name = "pnl_Pantry";
+            this.pnl_Pantry.Size = new System.Drawing.Size(250, 200);
+            this.pnl_Pantry.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(167)))), ((int)(((byte)(143)))));
-            this.label3.Location = new System.Drawing.Point(220, 485);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(179, 489);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(279, 56);
+            this.label3.Size = new System.Drawing.Size(224, 45);
             this.label3.TabIndex = 6;
             this.label3.Text = "CHECKLISTS";
             // 
@@ -175,10 +172,9 @@
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
             this.panel3.Controls.Add(this.button2);
-            this.panel3.Location = new System.Drawing.Point(253, 586);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(180, 571);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(303, 169);
+            this.panel3.Size = new System.Drawing.Size(227, 137);
             this.panel3.TabIndex = 8;
             // 
             // button2
@@ -186,10 +182,9 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(167)))), ((int)(((byte)(143)))));
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(101, 70);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(76, 57);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "+";
             this.button2.UseVisualStyleBackColor = false;
@@ -198,38 +193,86 @@
             // 
             this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(167)))), ((int)(((byte)(143)))));
             this.label4.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(240, 540);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(170, 534);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(329, 46);
+            this.label4.Size = new System.Drawing.Size(247, 37);
             this.label4.TabIndex = 7;
             this.label4.Text = "Add new checklist";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // flp_PantriesContainer
+            // 
+            this.flp_PantriesContainer.Controls.Add(this.pnl_Pantry);
+            this.flp_PantriesContainer.Location = new System.Drawing.Point(175, 262);
+            this.flp_PantriesContainer.Name = "flp_PantriesContainer";
+            this.flp_PantriesContainer.Size = new System.Drawing.Size(1204, 206);
+            this.flp_PantriesContainer.TabIndex = 10;
+            // 
+            // pnl_WinTitleAndControls
+            // 
+            this.pnl_WinTitleAndControls.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pnl_WinTitleAndControls.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(78)))), ((int)(((byte)(76)))));
+            this.pnl_WinTitleAndControls.Controls.Add(this.btn_DashboardMinimize);
+            this.pnl_WinTitleAndControls.Controls.Add(this.btn_DashboardClose);
+            this.pnl_WinTitleAndControls.Location = new System.Drawing.Point(0, 0);
+            this.pnl_WinTitleAndControls.Name = "pnl_WinTitleAndControls";
+            this.pnl_WinTitleAndControls.Size = new System.Drawing.Size(1414, 71);
+            this.pnl_WinTitleAndControls.TabIndex = 11;
+            this.pnl_WinTitleAndControls.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_WinTitleAndControls_MouseDown);
+            this.pnl_WinTitleAndControls.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnl_WinTitleAndControls_MouseMove);
+            this.pnl_WinTitleAndControls.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnl_WinTitleAndControls_MouseUp);
+            // 
+            // btn_DashboardMinimize
+            // 
+            this.btn_DashboardMinimize.BackColor = System.Drawing.Color.Lime;
+            this.btn_DashboardMinimize.FlatAppearance.BorderSize = 0;
+            this.btn_DashboardMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DashboardMinimize.Location = new System.Drawing.Point(38, 12);
+            this.btn_DashboardMinimize.Name = "btn_DashboardMinimize";
+            this.btn_DashboardMinimize.Size = new System.Drawing.Size(20, 20);
+            this.btn_DashboardMinimize.TabIndex = 12;
+            this.btn_DashboardMinimize.UseVisualStyleBackColor = false;
+            this.btn_DashboardMinimize.Click += new System.EventHandler(this.btn_DashboardMinimize_Click);
+            // 
+            // btn_DashboardClose
+            // 
+            this.btn_DashboardClose.BackColor = System.Drawing.Color.Red;
+            this.btn_DashboardClose.FlatAppearance.BorderSize = 0;
+            this.btn_DashboardClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DashboardClose.Location = new System.Drawing.Point(12, 12);
+            this.btn_DashboardClose.Name = "btn_DashboardClose";
+            this.btn_DashboardClose.Size = new System.Drawing.Size(20, 20);
+            this.btn_DashboardClose.TabIndex = 11;
+            this.btn_DashboardClose.UseVisualStyleBackColor = false;
+            this.btn_DashboardClose.Click += new System.EventHandler(this.btn_DashboardClose_Click);
+            // 
             // UserDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(228)))), ((int)(((byte)(216)))));
-            this.ClientSize = new System.Drawing.Size(1884, 806);
+            this.ClientSize = new System.Drawing.Size(1413, 720);
+            this.Controls.Add(this.pnl_WinTitleAndControls);
+            this.Controls.Add(this.flp_PantriesContainer);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.lbl_DasboardLogo);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "UserDashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UserDashboard";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pb_Checklist)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_UserPicture)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Pantry)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Dashboard)).EndInit();
-            this.panel2.ResumeLayout(false);
+            this.pnl_Pantry.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            this.flp_PantriesContainer.ResumeLayout(false);
+            this.pnl_WinTitleAndControls.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,11 +288,15 @@
         private System.Windows.Forms.PictureBox pb_UserPicture;
         private System.Windows.Forms.PictureBox pb_Pantry;
         private System.Windows.Forms.PictureBox pb_Dashboard;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_AddNewPantry;
+        private System.Windows.Forms.Panel pnl_Pantry;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FlowLayoutPanel flp_PantriesContainer;
+        private System.Windows.Forms.Panel pnl_WinTitleAndControls;
+        private System.Windows.Forms.Button btn_DashboardMinimize;
+        private System.Windows.Forms.Button btn_DashboardClose;
     }
 }
--- PantrylyDesktopApp/UserDashboard.cs
+++ PantrylyDesktopApp/UserDashboard.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SQLite;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,35 @@ namespace PantrylyDesktopApp
 {
     public partial class UserDashboard : Form
     {
+        private Panel pnl_newPantry;
+        private TextBox txt_newPantryName;
+        private Label lbl_newPantryName;
         string currentUser_id = "";
         public UserDashboard(string user_id)
         {
             currentUser_id = user_id;
             InitializeComponent();
+            // TODO: Make the rounded edges smoother (anti-aliasing?)
+            // Rounded corners for the form 
+            GraphicsPath path = new GraphicsPath();
+            int arcSize = 20;
+            Rectangle rect = new Rectangle(0, 0, Width, Height);
+            path.AddArc(rect.X, rect.Y, arcSize, arcSize, 180, 90);
+            path.AddArc(rect.X + rect.Width - arcSize, rect.Y, arcSize, arcSize, 270, 90);
+            path.AddArc(rect.X + rect.Width - arcSize, rect.Y + rect.Height - arcSize, arcSize, arcSize, 0, 90);
+            path.AddArc(rect.X, rect.Y + rect.Height - arcSize, arcSize, arcSize, 90, 90);
+            Region = new Region(path);
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            // make btnDashboardClose circle.
+            GraphicsPath closePath = new GraphicsPath();
+            closePath.AddEllipse(0, 0, btn_DashboardClose.Width, btn_DashboardClose.Height);
+            btn_DashboardClose.Region = new Region(closePath);
+
+            // make btnDashboardMinimize circle.
+            GraphicsPath minimizePath = new GraphicsPath();
+            minimizePath.AddEllipse(0, 0, btn_DashboardMinimize.Width, btn_DashboardMinimize.Height);
+            btn_DashboardMinimize.Region = new Region(minimizePath);
         }
 
         SQLiteConnection sql_con;
@@ -35,7 +60,6 @@ namespace PantrylyDesktopApp
         {
             sql_con = new SQLiteConnection("Data Source = PantrylyDB.db");
         }
-
 
         private void executeQuery(string txtQuery)
         {
@@ -62,5 +86,92 @@ namespace PantrylyDesktopApp
             
         }
 
+        private void btnAddNewPantry_Click(object sender, EventArgs e)
+        {
+            pnl_newPantry = new Panel();
+            pnl_newPantry.Size = new Size(250, 200);
+            pnl_newPantry.BorderStyle = BorderStyle.None;
+            pnl_newPantry.BackColor = ColorTranslator.FromHtml("#D9D9D9");
+
+            txt_newPantryName = new TextBox();
+            txt_newPantryName.Text = "Enter Pantry name...";
+            txt_newPantryName.Size = new Size(250, 37);
+            txt_newPantryName.Location = new Point(0, 0);
+
+            lbl_newPantryName = new Label();
+            lbl_newPantryName.Text = "New Pantry";
+            lbl_newPantryName.Size = new Size(250, 37);
+            lbl_newPantryName.BackColor = ColorTranslator.FromHtml("#D4664E");
+            lbl_newPantryName.Font = new Font("Comic Sans MS", 16, FontStyle.Regular);
+            lbl_newPantryName.TextAlign = ContentAlignment.MiddleCenter;
+
+            pnl_newPantry.Controls.Add(txt_newPantryName);     
+
+            // Add the new panel to the FlowLayoutPanel
+            DialogResult result = MessageBox.Show("Are you sure you want to create a new Pantry?", "New Pantry", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                flp_PantriesContainer.Controls.Add(pnl_newPantry);
+                txt_newPantryName.Focus();
+                /*
+                 * this is used to associate the KeyDown event handler with the KeyDown event of txt_newPantryName.KeyDown,
+                 * if this isn't used, nothing happens after the user presses enter
+                 */
+                txt_newPantryName.KeyDown += new KeyEventHandler(txt_newPantryName_KeyDown);
+            }
+        }
+
+        private void txt_newPantryName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string newName = txt_newPantryName.Text;
+                txt_newPantryName.Dispose();
+                if (!string.IsNullOrEmpty(newName))
+                {
+                    lbl_newPantryName.Text = newName;
+                    pnl_newPantry.Controls.Add(lbl_newPantryName);
+                    // add query to execute for adding the pantry to the table
+                }
+                else
+                {
+                    flp_PantriesContainer.Controls.Remove(pnl_newPantry);
+                }
+            }
+        }
+
+        // Variables to use for dragging around window
+        private bool isDragging = false;
+        private Point lastCursorPos;
+
+        private void pnl_WinTitleAndControls_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            lastCursorPos = Cursor.Position;
+        }
+
+        private void pnl_WinTitleAndControls_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point deltaCursorPos = new Point(Cursor.Position.X - lastCursorPos.X, Cursor.Position.Y - lastCursorPos.Y);
+                this.Location = new Point(this.Location.X + deltaCursorPos.X, this.Location.Y + deltaCursorPos.Y);
+                lastCursorPos = Cursor.Position;
+            }
+        }
+
+        private void pnl_WinTitleAndControls_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+        private void btn_DashboardClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_DashboardMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
     }
 }
